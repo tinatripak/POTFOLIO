5279eb1a764d2445167a5846aa1a899a7b38d6ca
Kristina Tripak <tinatripak2002@gmail.com>
2024-01-19 00:29:47 -0500

    fixed animation by scrolling

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,40 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { HiArrowLongDown } from "react-icons/hi2";
 import classes from "./About.module.scss";
 
 const About = () => {
+
+  const [isInViewport, setIsInViewport] = useState(false);
+  const aboutRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.1,
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setIsInViewport(true);
+          observer.disconnect(); 
+        }
+      });
+    }, options);
+
+    if (aboutRef.current) {
+      observer.observe(aboutRef.current);
+    }
+
+    return () => {
+      if (aboutRef.current) {
+        observer.unobserve(aboutRef.current);
+      }
+    };
+  }, []);
   return (
-    <section className={classes.about} id="about">
+    <section className={classes.about} id="about" ref={aboutRef} >
       <div className={classes.text}>
         <p className={classes.firstParagraph}>
           Hey, I'm Kristina Tripak, a recent graduate in Software Engineering
@@ -28,43 +58,43 @@ const About = () => {
       <div className={classes.rightBlock}>
         <div className={classes.chronology}>
 
-          <div className={classes.workExperience} >
+          <div className={`${classes.workExperience} ${isInViewport ? classes.fadeIn : ""}`} >
             <p className={classes.title}>Front-end Program</p>
             <span className={classes.time}>Apr 2022 - Jul 2022</span>
           </div>
-          <div className={classes.icon}>
+          <div className={`${classes.icon} ${isInViewport ? classes.fadeIn : ""}`}>
             <HiArrowLongDown size={40} />
           </div>
 
-          <div className={classes.workExperience}>
+          <div className={`${classes.workExperience} ${isInViewport ? classes.fadeIn : ""}`}>
             <p className={classes.title}>Harmony project</p>
             <span className={classes.time}>Mar 2023 - May 2023</span>
           </div>
-          <div className={classes.icon}>
+          <div className={`${classes.icon} ${isInViewport ? classes.fadeIn : ""}`}>
             <HiArrowLongDown size={40} />
           </div>
 
-          <div className={classes.workExperience}>
+          <div className={`${classes.workExperience} ${isInViewport ? classes.fadeIn : ""}`}>
             <p className={classes.title}>
               Bachelor's degree of Software Engineering
             </p>
             <span className={classes.time}>Sep 2019 - Jun 2023</span>
           </div>
-          <div className={classes.icon}>
+          <div className={`${classes.icon} ${isInViewport ? classes.fadeIn : ""}`}>
             <HiArrowLongDown size={40} />
           </div>
 
-          <div className={classes.workExperience}>
+          <div className={`${classes.workExperience} ${isInViewport ? classes.fadeIn : ""}`}>
             <p className={classes.title}>
             Practical React marathon
             </p>
             <span className={classes.time}>Aug 2022 - Nov 2023</span>
           </div>
-          <div className={classes.icon}>
+          <div className={`${classes.icon} ${isInViewport ? classes.fadeIn : ""}`}>
             <HiArrowLongDown size={40} />
           </div>
 
-          <div className={classes.workExperience}>
+          <div className={`${classes.workExperience} ${isInViewport ? classes.fadeIn : ""}`}>
             <p className={classes.title}>
             Ksigallery project
             </p>
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import classes from "./Skills.module.scss";
 
 const Skills = () => {
+  const [isInViewport, setIsInViewport] = useState(false);
+  const skillsRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.1,
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setIsInViewport(true);
+          observer.disconnect(); 
+        }
+      });
+    }, options);
+
+    if (skillsRef.current) {
+      observer.observe(skillsRef.current);
+    }
+
+    return () => {
+      if (skillsRef.current) {
+        observer.unobserve(skillsRef.current);
+      }
+    };
+  }, []);
   return (
-    <section className={classes.skills} id="skills">
+    <section className={classes.skills} id="skills" ref={skillsRef}>
       <h2 className={classes.heading}>Skills</h2>
 
-      <div className={classes.content}>
-        <div className={`${classes.mongoIcon} ${classes.icon}`}>
+      <div className={`${classes.content}`}>
+        <div className={`${classes.mongoIcon} ${classes.icon}  ${isInViewport ? classes.fadeIn : ""}`}>
           <img
             src={process.env.PUBLIC_URL + "/images/mongoIcon.png"}
             alt="mongo icon"
@@ -15,25 +44,25 @@ const Skills = () => {
         </div>
         <div className={classes.orangeIcon}></div>
         <div className={classes.purpleIcon}></div>
-        <div className={`${classes.tailwindIcon} ${classes.icon}`}>
+        <div className={`${classes.tailwindIcon} ${classes.icon}  ${isInViewport ? classes.fadeIn : ""}`}>
           <img
             src={process.env.PUBLIC_URL + "/images/tailwindIcon.png"}
             alt="tailwind icon"
           />
         </div>
-        <div className={`${classes.basicIcon} ${classes.icon}`}>
+        <div className={`${classes.basicIcon} ${classes.icon}  ${isInViewport ? classes.fadeIn : ""}`}>
           <img
             src={process.env.PUBLIC_URL + "/images/basicIcon.png"}
             alt="html css js icon"
           />
         </div>
-        <div className={`${classes.reactIcon} ${classes.icon}`}>
+        <div className={`${classes.reactIcon} ${classes.icon}  ${isInViewport ? classes.fadeIn : ""}`}>
           <img
             src={process.env.PUBLIC_URL + "/images/reactIcon.png"}
             alt="react icon"
           />
         </div>
-        <div className={`${classes.scssIcon} ${classes.icon}`}>
+        <div className={`${classes.scssIcon} ${classes.icon}  ${isInViewport ? classes.fadeIn : ""}`}>
           <img
             src={process.env.PUBLIC_URL + "/images/scssIcon.png"}
             alt="scss icon"
@@ -41,32 +70,32 @@ const Skills = () => {
         </div>
         <div className={classes.lightPinkIcon}></div>
         <div className={classes.darkPinkIcon}></div>
-        <div className={`${classes.nodeIcon} ${classes.icon}`}>
+        <div className={`${classes.nodeIcon} ${classes.icon}  ${isInViewport ? classes.fadeIn : ""}`}>
           <img
             src={process.env.PUBLIC_URL + "/images/nodeIcon.png"}
             alt="node icon"
           />
         </div>
-        <div className={`${classes.postmanIcon} ${classes.icon}`}>
+        <div className={`${classes.postmanIcon} ${classes.icon}  ${isInViewport ? classes.fadeIn : ""}`}>
           <img
             src={process.env.PUBLIC_URL + "/images/postmanIcon.png"}
             alt="postman icon"
           />
         </div>
-        <div className={`${classes.expressIcon} ${classes.icon}`}>
+        <div className={`${classes.expressIcon} ${classes.icon}  ${isInViewport ? classes.fadeIn : ""}`}>
           <img
             src={process.env.PUBLIC_URL + "/images/expressIcon.png"}
             alt="express icon"
           />
         </div>
         <div className={classes.pinkIcon}></div>
-        <div className={`${classes.figmaIcon} ${classes.icon}`}>
+        <div className={`${classes.figmaIcon} ${classes.icon}  ${isInViewport ? classes.fadeIn : ""}`}>
           <img
             src={process.env.PUBLIC_URL + "/images/figmaIcon.png"}
             alt="figma icon"
           />
         </div>
-        <div className={`${classes.bootstrapIcon} ${classes.icon}`}>
+        <div className={`${classes.bootstrapIcon} ${classes.icon} ${isInViewport ? classes.fadeIn : ""}`}>
           <img
             src={process.env.PUBLIC_URL + "/images/bootstrapIcon.png"}
             alt="bootstrap icon"
