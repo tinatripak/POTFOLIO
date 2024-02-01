5067461d29f59194ea44b76f66a1d9e9df07bcc1
Kristina Tripak <tinatripak2002@gmail.com>
2024-01-31 22:24:59 -0500

    added two projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -11,21 +11,33 @@ const Projects = () => {
           <div className={classes.photoBlock}>
             <img
               src={process.env.PUBLIC_URL + "/images/firstProject.png"}
-              alt="First project"
+              alt="Harmony project"
               className={classes.firstPhoto}
             />
             <div className={classes.buttons}>
-              <a href='https://github.com/tinatripak/HarmonyMusicClient'className={classes.liveCodeButton}>Live code</a>
-              <a href='https://harmony-music-client.vercel.app/login'className={classes.demoButton} >Demo</a>
+              <a
+                href="https://github.com/tinatripak/HarmonyMusicClient"
+                className={classes.liveCodeButton}
+              >
+                Live code
+              </a>
+              <a
+                href="https://harmony-music-client.vercel.app/login"
+                className={classes.demoButton}
+              >
+                Demo
+              </a>
             </div>
           </div>
           <p className={classes.description}>
             Discover and enjoy Ukrainian music on our convenient platform. Find
             artists, filter music and create personalized playlists. Discover
             your favorite songs and edit your profile. Immerse yourself in the
-            bright world of Ukrainian melody!<br/><br/>The site was built using the{" "}
-            <span>MERN stack</span>, and I also used <span>Firebase</span> to
-            authorize and store photos and music.
+            bright world of Ukrainian melody!
+            <br />
+            <br />
+            The site was built using the <span>MERN stack</span>, and I also
+            used <span>Firebase</span> to authorize and store photos and music.
           </p>
         </div>
 
@@ -35,19 +47,30 @@ const Projects = () => {
             There you can view photo shoots made by this photographer and book a
             session right on the site. Check out the available types of photo
             shoots and transparent prices. Contact the photographer to ask any
-            question you are interested in. <br/><br/>The website was built using the{" "}
-            <span>MERN stack</span>, and for storing photos in the database, I
-            utilized <span>Cloudinary</span>.
+            question you are interested in. <br />
+            <br />
+            The website was built using the <span>MERN stack</span>, and for
+            storing photos in the database, I utilized <span>Cloudinary</span>.
           </p>
           <div className={classes.photoBlock}>
             <img
               src={process.env.PUBLIC_URL + "/images/secondProject.png"}
-              alt="Second project"
+              alt="KSIGALLERY project"
               className={classes.secondPhoto}
             />
             <div className={classes.buttons}>
-              <a href='https://github.com/tinatripak/photograph-portfolio-client'className={classes.liveCodeButton}>Live code</a>
-              <a href='https://ksigallery.vercel.app'className={classes.demoButton} >Demo</a>
+              <a
+                href="https://github.com/tinatripak/photograph-portfolio-client"
+                className={classes.liveCodeButton}
+              >
+                Live code
+              </a>
+              <a
+                href="https://ksigallery.vercel.app"
+                className={classes.demoButton}
+              >
+                Demo
+              </a>
             </div>
           </div>
           <p className={classes.title}>KSIGALLERY</p>
@@ -57,18 +80,30 @@ const Projects = () => {
             This is a one-page website with responsive design, navigation, a
             header consisting of a photo, menu, and text, a main block in the
             form of recommended products with prices, product categories, and a
-            footer in the form of a photo. It was a project from the course. <br/><br/>The
-            web page was built using <span>React.js library</span>.
+            footer in the form of a photo. It was a project from the course.{" "}
+            <br />
+            <br />
+            The web page was built using <span>React.js library</span>.
           </p>
           <div className={classes.photoBlock}>
             <img
               src={process.env.PUBLIC_URL + "/images/thirdProject.png"}
-              alt="Third project"
+              alt="BOSE APP project"
               className={classes.thirdPhoto}
             />
             <div className={classes.buttons}>
-              <a href='https://github.com/tinatripak/bose-react' className={classes.liveCodeButton}>Live code</a>
-              <a href='https://bose-react.vercel.app'className={classes.demoButton} >Demo</a>
+              <a
+                href="https://github.com/tinatripak/bose-react"
+                className={classes.liveCodeButton}
+              >
+                Live code
+              </a>
+              <a
+                href="https://bose-react.vercel.app"
+                className={classes.demoButton}
+              >
+                Demo
+              </a>
             </div>
           </div>
           <p className={classes.title}>BOSE APP</p>
@@ -79,20 +114,93 @@ const Projects = () => {
           <div className={classes.photoBlock}>
             <img
               src={process.env.PUBLIC_URL + "/images/fourthProject.png"}
-              alt="Fourth project"
+              alt="Auth web page project"
               className={classes.fourthPhoto}
             />
             <div className={classes.buttons}>
-              <a href='https://github.com/tinatripak/HarmonyMusicClient'className={classes.liveCodeButton}>Live code</a>
-              <a href='https://harmony-music-client.vercel.app/login'className={classes.demoButton} >Demo</a>
+              <a
+                href="https://github.com/tinatripak/HarmonyMusicClient"
+                className={classes.liveCodeButton}
+              >
+                Live code
+              </a>
+              <a
+                href="https://harmony-music-client.vercel.app/login"
+                className={classes.demoButton}
+              >
+                Demo
+              </a>
             </div>
           </div>
           <p className={classes.description}>
             This is an authentication page that uses Firebase for Google
-            Authentication and authorization with email and password.<br/><br/>The web
-            page was built using <span>React.js library</span> and{" "}
+            Authentication and authorization with email and password.
+            <br />
+            <br />
+            The web page was built using <span>React.js library</span> and{" "}
             <span>Firebase</span>.
           </p>
+        </div>
+
+        <div className={`${classes.project} ${classes.firstProject}`}>
+          <p className={classes.title}>BookStore Redux</p>
+          <div className={classes.photoBlock}>
+            <img
+              src={process.env.PUBLIC_URL + "/images/bookstore.png"}
+              alt="Bookstore project"
+              className={classes.firstPhoto}
+            />
+            <div className={classes.buttons}>
+              <a
+                href="https://github.com/tinatripak/BookStoreRedux"
+                className={classes.liveCodeButton}
+              >
+                Live code
+              </a>
+              <a
+                href="https://bookstore-redux.vercel.app/"
+                className={classes.demoButton}
+              >
+                Demo
+              </a>
+            </div>
+          </div>
+          <p className={classes.description}>
+            This project was creating using <span>Redux</span> and{" "}
+            <span>React</span> libraries to store books with id, name, price,
+            category and description values in json and manipulate data like
+            add, update and delete using reducers.
+          </p>
+        </div>
+
+        <div className={`${classes.project} ${classes.secondProject}`}>
+          <p className={classes.description}>
+            This project was created using <span>Redux</span> and <span>React</span> libraries to be used
+            in my future yoga website with videos and yoga plans. <span>Redux</span> was used
+            to get information from the server and store it.
+          </p>
+          <div className={classes.photoBlock}>
+            <img
+              src={process.env.PUBLIC_URL + "/images/yoga.png"}
+              alt="Yoga project"
+              className={classes.secondPhoto}
+            />
+            <div className={classes.buttons}>
+              <a
+                href="https://github.com/tinatripak/YogaReduxClient"
+                className={classes.liveCodeButton}
+              >
+                Live code
+              </a>
+              <a
+                href="https://yoga-redux.vercel.app/"
+                className={classes.demoButton}
+              >
+                Demo
+              </a>
+            </div>
+          </div>
+          <p className={classes.title}>Yoga Redux</p>
         </div>
       </div>
     </section>
