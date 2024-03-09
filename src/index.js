ec091a674b6350524e80addaeaea7c262f179965
Kristina Tripak <tinatripak2002@gmail.com>
2024-03-08 21:01:22 -0500

    added analytics

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Analytics } from "@vercel/analytics/react";
 
 import "./styles/index.scss";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+    <Analytics />
     <App />
   </BrowserRouter>
 );
